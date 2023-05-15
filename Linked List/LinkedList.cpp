557aaf8aaadff21b3b9dfeea139ff54ea0a7e1ab
Chiko Satria <75495316+chiknwy@users.noreply.github.com>
2023-05-16 03:03:49 +0800

    Hope it works :(

---
+++ Linked List/LinkedList.cpp
@@ -0,0 +1,76 @@
+#include <iostream>
+#include "LinkedList.h"
+
+
+void LinkedList::insertToHead(int input){
+	Node *newNode = new Node();
+	newNode->val= input;
+	
+	newNode->next= head;
+	head = newNode;
+	
+	if(tail==NULL)
+	tail=head;
+}
+
+void LinkedList::printAll(){
+	Node *tmp = head;
+	while (tmp==NULL){
+		std::cout<<"Linked List is Empty!!";
+		break;
+	}
+	while(tmp!=NULL){
+		std::cout<<tmp->val<<"->";
+		tmp = tmp->next;
+	}
+	std::cout<<std::endl;
+}
+
+void LinkedList::insertToTail(int input){
+	Node *newNode = new Node();
+	newNode->val= input;
+	
+	if (head == NULL) {
+        std::cout << "Error!! List is empty. Cannot insert to tail because there is no tail." << std::endl;
+        delete newNode;
+    }
+    else{
+	
+	Node *tmp= tail;
+	tail->next = newNode;
+	tail=newNode;
+	}
+}
+
+void LinkedList::insertAfter(int x, int b){
+	Node *newNode = new Node();
+    	newNode->val = b;
+
+    	Node *tmp = head;
+    	bool checker = false;
+    	if (head == NULL) {
+        std::cout << "Error!! List is empty. Cannot insert after." << std::endl;
+        delete newNode;
+    }
+
+    	while(tmp != NULL){
+            if(tmp->val == x){
+            	newNode->next = tmp->next;
+            	tmp->next = newNode;
+            if(tmp == tail){
+                tail = newNode;
+            }
+            
+            checker = true;
+            return;
+        }
+        tmp = tmp->next;
+    }
+    
+    	if(checker == false){
+    		std::cout << "Error: Value " << x << " not found in the list. Cannot insert after." << std::endl;
+        delete newNode;
+		}
+	
+	
+}
