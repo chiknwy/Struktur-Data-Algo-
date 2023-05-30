cb173fa7b6bee307410e49d14b2b5aaf8a706073
Chiko Satria <75495316+chiknwy@users.noreply.github.com>
2023-05-31 02:18:37 +0800

    Update Main.cpp

--- Stack/Main.cpp
+++ Stack/Main.cpp
@@ -22,21 +22,21 @@ int main() {
 	Stack userstack1;
 	while (vals != 4) {
 		
-		std::cout << "\n----------Stack----------\n";
-		std::cout << "1. Push\n";
-		std::cout << "2. Pop\n";
-		std::cout << "3. Print All\n";
-		std::cout << "4. Exit\n";
+		cout << "\n----------Stack----------\n";
+		cout << "1. Push\n";
+		cout << "2. Pop\n";
+		cout << "3. Print All\n";
+		cout << "4. Exit\n";
 		
-		std::cout << "Input: ";
-		std::cin >> vals;
+		cout << "Input: ";
+		cin >> vals;
 			
 		switch (vals) {
 			
 			case 1: {
 				int val;
-				std::cout << "Push: ";
-				std::cin >> val;
+				cout << "Push: ";
+				cin >> val;
 				userstack1.push(val);
 				userstack1.printAll();
 				break;
@@ -45,12 +45,12 @@ int main() {
 			case 2: {
 				Node* tmp = userstack1.top;
 				if (tmp != NULL) {
-					std::cout << "Popped Value: " << tmp->value << std::endl;
+					cout << "Popped Value: " << tmp->value << std::endl;
 					userstack1.pop();
 				} 
 				
 				else {
-					std::cout << "Stack is empty. Cannot pop." << std::endl;
+					cout << "Stack is empty. Cannot pop." << std::endl;
 				}
 				userstack1.printAll();
 				break;
@@ -61,11 +61,11 @@ int main() {
 				break;
 					
 			case 4:
-				std::cout << "Exiting Program..." << std::endl;
+				cout << "Exiting Program..." << std::endl;
 				break;
 
 			default:
-				std::cout << "Please input 1-4!" << std::endl;
+				cout << "Please input 1-4!" << std::endl;
 				break;
 					
 		}		
